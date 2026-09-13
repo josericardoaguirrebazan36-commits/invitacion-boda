efe9091533d8b15a38b5b30a06a1ffe70a66a33a
josericardoaguirrebazan36-commits <josericardoaguirrebazan36@gmail.com>
2026-09-12 23:16:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1269,9 +1269,8 @@ function obtenerMensajeConfirmacion(
                 personas === 1
                     ? "persona"
                     : "personas"
-            }. ¡Nos alegra mucho contar contigo! Si hubo algún error en tu respuesta o deseas cambiarla,
-                comunícate con Omar Ulloa al
-                +51 992 418 572.`;
+            }. ¡Nos alegra mucho contar contigo!
+            `;
 
 
         case "zoom":
